0dc5789ac7804f1d6c759133cbd5f1f35f5dd035
John Bintz <john@coswellproductions.com>
2013-02-20 13:23:10 -0500

    fix rake task

--- lib/seed-fu-mongoid/railtie.rb
+++ lib/seed-fu-mongoid/railtie.rb
@@ -3,7 +3,7 @@ module SeedFuMongoid
     rake_tasks do
       namespace :db do
         desc "Load seed data from db/fixtures"
-        task :seed_fu do
+        task :seed_fu => :environment do
           Dir['db/fixtures/**/*.rb'].each do |file|
             puts "== Seed from #{file}"
 
